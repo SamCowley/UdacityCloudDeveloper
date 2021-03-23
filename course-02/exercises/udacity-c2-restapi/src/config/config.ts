661d4bdb529613c3e815dc8411f574222b20236f
Sam Cowley <samuel.alexander.cowley@gmail.com>
2021-03-23 01:27:11 -0400

    Squashed commit of the following:
    commit b42d9d4d17e69e6a19f7a6f620f9df18519ea823
    Author: Sam Cowley <samuel.alexander.cowley@gmail.com>
    Date:   Tue Mar 23 01:20:07 2021 -0400
    
        Add authentication system
    
    commit 23b8a3bd33beb341a98b0386e22b29f177cb00cc
    Author: Sam Cowley <samuel.alexander.cowley@gmail.com>
    Date:   Mon Mar 22 22:53:21 2021 -0400
    
        Correct gitignore location
    
    commit 30cfadbec5d684695e8f25c02019e7e38c363e36
    Author: Sam Cowley <samuel.alexander.cowley@gmail.com>
    Date:   Mon Mar 22 22:50:04 2021 -0400
    
        Hide credentials and complete JWT stub
    
    commit 190023dcf3bada4b57320f8aa4a4c20916c0bbbd
    Author: Sam Cowley <samuel.alexander.cowley@gmail.com>
    Date:   Mon Mar 22 22:28:45 2021 -0400
    
        Complete password comparison example
    
    commit 8d91c9ab0b79767b6a8f675273ee97b5fdf37d57
    Author: Sam Cowley <samuel.alexander.cowley@gmail.com>
    Date:   Mon Mar 22 22:05:49 2021 -0400
    
        Initial Configuration for the REST API example
    
    commit 369096d7bde3e41e43de7db0e45a50a2ad171ea4
    Author: Sam Cowley <samuel.alexander.cowley@gmail.com>
    Date:   Mon Mar 22 22:00:35 2021 -0400
    
        Add cars endpoint to the basic server

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,25 @@
 export const config = {
   "dev": {
-    "username": "udagramcowleydev",
-    "password": "udagramcowleydev",
-    "database": "udagramcowleydev",
-    "host": "udagramcowleydev.cmmwfio3stmb.us-east-1.rds.amazonaws.com",
+    "username": process.env.dev_username,
+    "password": process.env.dev_password,
+    "database": process.env.dev_database,
+    "host": process.env.dev_host,
     "dialect": "postgres",
-    "aws_region": "us-east-1",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagramscowleydev"
+    "aws_region": process.env.dev_region,
+    "aws_profile": process.env.dev_profile,
+    "aws_media_bucket": process.env.dev_media_bucket
   },
   "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
+    "username": process.env.prod_username,
+    "password": process.env.prod_password,
+    "database": process.env.prod_database,
+    "host": process.env.prod_host,
+    "dialect": "postgres",
+    "aws_region": process.env.prod_region,
+    "aws_profile": process.env.prod_profile,
+    "aws_media_bucket": process.env.prod_media_bucket
+  },
+  "jwt": {
+    "secret": process.env.jwt_secret
   }
 }
